935d678d12fc4674404bf1f2a6d0974733260383
wugui2020 <momi2020@uw.edu>
2016-11-08 15:10:27 -0800

    Session Object Init

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
+import MySQLdb
 BASE = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def id_encoder(num, base = BASE):
-
-    if not num:
-        return BASE[0]
-
-    ans = []
-    length = len(base)
-
-    while num:
-        ans += base[num % length],
-        num /= length
-
-    return "".join(ans)
-
-def id_decoder(code, base = BASE):
-
-    ans = 0
-    length = len(base)
-
-    for i, char in enumerate(code):
-        ans += base.index(char) * length ** i
-
-    return ans
     
 
+class Session():
+    
+    def __init__(self):
+        """
+        Session Initialization
+        """
+        user, passwd = self.getUserCredentials()
+        self.db = MySQLdb.connect(user = user, passwd = passwd)
+
+    
+    def id_encoder(self, num, base = BASE):
+        """
+        Take an number and return an shortened string representation.
+        Input: Int
+        Output: String
+        """
+
+        if not num:
+            return BASE[0]
+
+        ans = []
+        length = len(base)
+
+        while num:
+            ans += base[num % length],
+            num /= length
+
+        return "".join(ans)
+
+    def id_decoder(self, code, base = BASE):
+        """
+        Take an encoded string and reverse it back to the original number.
+        Input: String
+        Output: Int
+        """
+
+        ans = 0
+        length = len(base)
+
+        for i, char in enumerate(code):
+            ans += base.index(char) * length ** i
+
+        return ans
+
+
+    
 
 
